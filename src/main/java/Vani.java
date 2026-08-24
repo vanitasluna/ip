fd4aa11e47d0545cd64bb363ab831a4556ecc00b
Phi Hung <e1528402@u.nus.edu>
2026-08-25 01:11:08 +0800

    Level-0

--- src/main/java/Vani.java
+++ src/main/java/Vani.java
@@ -1,7 +1,11 @@
 public class Vani {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
-        System.out.println("[CHATBOT BANNER]");
+        System.out.println("|-------------------------|");
+        System.out.println("  \\    /   /\\   |\\  |  |");
+        System.out.println("   \\  /   /__\\  | \\ |  |");
+        System.out.println("    \\/    |  |  |  \\|  |");
+        System.out.println("|-------------------------|");
         System.out.println("Hello! I'm Vani.");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
