cf772e2226a82f46cf576acdde26d52f539ab022
Phi Hung <e1528402@u.nus.edu>
2026-08-26 18:37:06 +0800

    level-1

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Vani User Guide
 
 // Update the title above to match the actual product name
 
--- src/main/java/Vani.java
+++ src/main/java/Vani.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Vani {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
@@ -9,7 +11,21 @@ public class Vani {
         System.out.println("Hello! I'm Vani.");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                String command = scanner.nextLine();
+                System.out.println("____________________________________________________________");
+
+                if (command.equals("bye")) {
+                    System.out.println("     Bye. Hope to see you again soon!");
+                    System.out.println("____________________________________________________________");
+                    break;
+                }
+
+                System.out.println("     " + command);
+                System.out.println("____________________________________________________________");
+            }
+        }
     }
 }
